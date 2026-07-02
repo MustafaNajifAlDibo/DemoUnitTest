d2145d479eaa07b0c01700fb4173a36fe9ac9343
Mustafa Najif Al-Dibo <mustafa.najif@outlook.com>
2026-07-02 20:39:31 +0200

    Create Library Project and install XUnit Package

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DemoLibrary.Tests {
+    public class CalculatorTests {
+
+        [Fact]
+        public void Add_ShouldReturnCorrectSum() {
+            // Arrange (Expected)
+            double expected = 8;
+            // Act (Actual)
+            double result = Calculator.Add(5, 3);
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
